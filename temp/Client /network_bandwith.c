c4528366dcb5516405beaeef4160cf0e3700a867
thedushn <thedushn@email.com>
2017-11-21 11:43:47 +0100

    slow but steddy

---
+++ temp/Client /network_bandwith.c
@@ -0,0 +1,348 @@
+//
+// Created by dushn on 16.8.17..
+//
+
+#include "network_bandwith.h"
+
+
+static int broj;
+char  name_buffer[10];
+int interface_name(){
+
+   /* int len;
+    int c;*/
+    char *v="/sys/class/net/";
+    struct dirent *pDirent;
+    DIR *pDir;
+   // char buffer[10];
+
+    pDir = opendir (v);
+    if (pDir == NULL) {
+        printf ("Cannot open directory '%s'\n", v);
+        return 1;
+    }
+
+    while ((pDirent = readdir(pDir)) != NULL) {
+       // printf ("[%s]\n", pDirent->d_name);
+        if(!strcmp(pDirent->d_name, ".") || !strcmp(pDirent->d_name, "..") || !strcmp(pDirent->d_name, "lo")){
+
+
+        }
+        else
+        sscanf(pDirent->d_name,"%s" ,name_buffer);
+
+    }
+
+ //   printf("NAME BUFFER : %s \n",name_buffer);
+
+
+    closedir (pDir);
+    return 0;
+
+}
+
+Network received_transfered(){
+
+        Network network_struct;
+
+  static  unsigned  long received_bytes1=0;
+  static  unsigned  long transmit_bytes1=0;
+
+   static unsigned  long received_bytes_old=0;
+   static unsigned  long transmit_bytes_old=0;
+
+    unsigned  long received_bytes=0;
+    unsigned  long received_packets=0;
+    unsigned  long received_errors;
+    unsigned  long received_drop;
+    unsigned  long received_fifo;
+    unsigned  long received_frame;
+    unsigned  long received_compressed;
+    unsigned  long received_multicast;
+    unsigned  long transmit_bytes=0;
+    unsigned  long transmit_packets=0;
+    unsigned  long transmit_errors;
+    unsigned  long transmit_drop;
+    unsigned  long transmit_fifo;
+    unsigned  long transmit_frame;
+    unsigned  long transmit_compressed;
+    unsigned  long transmit_multicast;
+    unsigned  long network=0 ;
+    unsigned  long network2=0;
+    unsigned  long network_rc =0;
+    unsigned  long network_ts = 0;
+    FILE *file;
+    char buffer[1024];
+    char buffer2[1024];
+    char buffer3[10];
+    char *network_size_rc=NULL;
+    char *network_size_ts=NULL;
+
+
+    strncpy(buffer3,name_buffer,sizeof(buffer3));
+
+
+    for(int i= 0;i<sizeof(buffer3);i++ ){
+
+        if (buffer3[i]==0)
+        break;
+        else
+            broj++;
+    }
+    char *filename = "/proc/net/dev";
+
+   char ch =' ';
+    int a=0;
+
+    if ((file = fopen (filename, "r")) == NULL || fgets (buffer, 1024, file) == NULL)
+        exit(1);
+
+    while (fgets (buffer, 1024, file) != NULL){
+
+
+        for(int i=0 ;i<broj;i++){
+
+           if (buffer[i]==buffer3[i])
+               a++;
+        }
+            if (a==broj) {
+                broj=0;
+                break;
+            }
+
+
+
+
+
+    }
+
+
+     //   printf("NET buffer :%s",buffer);
+
+
+
+   // printf("NET buffer :%s",buffer);
+
+    char *j=strchr(buffer,ch);
+
+   /* sscanf(buffer," %lu %lu",&received_bytes, &received_packets
+            , &received_errors,&received_drop, &received_fifo, &received_frame, &received_compressed, &received_multicast,
+    &transmit_bytes,
+    &transmit_packets,
+    &transmit_errors,
+    &transmit_drop,
+    &transmit_fifo,
+    &transmit_frame,
+    &transmit_compressed,
+   &transmit_multicast);*/
+   strncpy ( buffer2, j, sizeof(buffer2) );
+   // printf("buffer2 %s",buffer2);
+
+    sscanf(buffer2," %lu %lu  %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",&received_bytes, &received_packets,
+    &received_errors,&received_drop, &received_fifo,
+    &received_frame, &received_compressed,
+    &received_multicast,
+           &transmit_bytes,
+           &transmit_packets,
+           &transmit_errors,
+           &transmit_drop,
+           &transmit_fifo,
+           &transmit_frame,
+           &transmit_compressed,
+           &transmit_multicast);
+
+    fclose(file);
+    received_bytes_old= received_bytes1;
+    transmit_bytes_old= transmit_bytes1;
+
+    received_bytes1=received_bytes;
+    transmit_bytes1=transmit_bytes;
+
+
+
+    transmit_bytes= (transmit_bytes1- transmit_bytes_old);
+    received_bytes= (received_bytes1-received_bytes_old);
+
+  //  printf(" %li %li %li %li %li %li\n",transmit_bytes ,received_bytes,received_bytes1,transmit_bytes1,transmit_bytes_old,received_bytes_old);
+    network_rc = /*(float)*/received_bytes;
+    network_ts = /*(float)*/transmit_bytes;
+
+
+
+    static bool pocetak=false;
+    if (pocetak==false){
+        network_rc=0;
+        network=0;
+    pocetak = true;
+    }
+    /*else{
+        network_rc=network_rc/1000;
+    }*/
+    //network_rc=network_rc/1024;}
+    static bool pocetak2=false;
+    if (pocetak2==false){
+        network_ts=0;
+        network2=0;
+    pocetak2 = true;}
+
+    network_struct.received_bytes=network_rc;
+    network_struct.transmited_bytes=network_ts;
+
+    return network_struct;
+
+
+    }
+
+
+
+void received_transfered2(Network *network_st){
+
+
+
+    static  unsigned  long received_bytes1=0;
+    static  unsigned  long transmit_bytes1=0;
+
+    static unsigned  long received_bytes_old=0;
+    static unsigned  long transmit_bytes_old=0;
+
+    unsigned  long received_bytes=0;
+    unsigned  long received_packets=0;
+    unsigned  long received_errors;
+    unsigned  long received_drop;
+    unsigned  long received_fifo;
+    unsigned  long received_frame;
+    unsigned  long received_compressed;
+    unsigned  long received_multicast;
+    unsigned  long transmit_bytes=0;
+    unsigned  long transmit_packets=0;
+    unsigned  long transmit_errors;
+    unsigned  long transmit_drop;
+    unsigned  long transmit_fifo;
+    unsigned  long transmit_frame;
+    unsigned  long transmit_compressed;
+    unsigned  long transmit_multicast;
+    unsigned  long network=0 ;
+    unsigned  long network2=0;
+    unsigned  long network_rc =0;
+    unsigned  long network_ts = 0;
+    FILE *file;
+    char buffer[1024];
+    char buffer2[1024];
+    char buffer3[10];
+
+
+
+    strncpy(buffer3,name_buffer,sizeof(buffer3));
+
+
+    for(int i= 0;i<sizeof(buffer3);i++ ){
+
+        if (buffer3[i]==0)
+            break;
+        else
+            broj++;
+    }
+    char *filename = "/proc/net/dev";
+
+    char ch =' ';
+    int a=0;
+
+    if ((file = fopen (filename, "r")) == NULL || fgets (buffer, 1024, file) == NULL)
+        exit(1);
+
+    while (fgets (buffer, 1024, file) != NULL){
+
+
+        for(int i=0 ;i<broj;i++){
+
+            if (buffer[i]==buffer3[i])
+                a++;
+        }
+        if (a==broj) {
+            broj=0;
+            break;
+        }
+
+
+
+
+
+    }
+
+
+    //   printf("NET buffer :%s",buffer);
+
+
+
+    // printf("NET buffer :%s",buffer);
+
+    char *j=strchr(buffer,ch);
+
+    /* sscanf(buffer," %lu %lu",&received_bytes, &received_packets
+             , &received_errors,&received_drop, &received_fifo, &received_frame, &received_compressed, &received_multicast,
+     &transmit_bytes,
+     &transmit_packets,
+     &transmit_errors,
+     &transmit_drop,
+     &transmit_fifo,
+     &transmit_frame,
+     &transmit_compressed,
+    &transmit_multicast);*/
+    strncpy ( buffer2, j, sizeof(buffer2) );
+    // printf("buffer2 %s",buffer2);
+
+    sscanf(buffer2," %lu %lu  %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",&received_bytes, &received_packets,
+           &received_errors,&received_drop, &received_fifo,
+           &received_frame, &received_compressed,
+           &received_multicast,
+           &transmit_bytes,
+           &transmit_packets,
+           &transmit_errors,
+           &transmit_drop,
+           &transmit_fifo,
+           &transmit_frame,
+           &transmit_compressed,
+           &transmit_multicast);
+
+    fclose(file);
+    received_bytes_old= received_bytes1;
+    transmit_bytes_old= transmit_bytes1;
+
+    received_bytes1=received_bytes;
+    transmit_bytes1=transmit_bytes;
+
+
+
+    transmit_bytes= (transmit_bytes1- transmit_bytes_old);
+    received_bytes= (received_bytes1-received_bytes_old);
+
+    //  printf(" %li %li %li %li %li %li\n",transmit_bytes ,received_bytes,received_bytes1,transmit_bytes1,transmit_bytes_old,received_bytes_old);
+    network_rc = /*(float)*/received_bytes;
+    network_ts = /*(float)*/transmit_bytes;
+
+
+
+    static bool pocetak=false;
+    if (pocetak==false){
+        network_rc=0;
+        network=0;
+        pocetak = true;
+    }
+    /*else{
+        network_rc=network_rc/1000;
+    }*/
+    //network_rc=network_rc/1024;}
+    static bool pocetak2=false;
+    if (pocetak2==false){
+        network_ts=0;
+        network2=0;
+        pocetak2 = true;}
+
+    network_st->received_bytes=network_rc;
+   network_st->transmited_bytes=network_ts;
+
+
+
+
+}
+
---
+++ temp/cmake-build-debug/CMakeFiles/gtkworld.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtkworld.dir/main.c.o"
+  "CMakeFiles/gtkworld.dir/interrupts.c.o"
+  "CMakeFiles/gtkworld.dir/drawing.c.o"
+  "CMakeFiles/gtkworld.dir/buttons.c.o"
+  "CMakeFiles/gtkworld.dir/testing_tree.c.o"
+  "CMakeFiles/gtkworld.dir/window.c.o"
+  "CMakeFiles/gtkworld.dir/functions.c.o"
+  "gtkworld.pdb"
+  "gtkworld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtkworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
